d7b270d2eac33ed74ef36c9f374017a8bb053777
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2023-01-06 17:06:07 +0100

    feat : generatur CRUD

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000
+const connectDB = require('./config/db');
+const models = require('./models')
 
+connectDB()
 
- 
-app.listen(port,console.log(`port ${port} runinig`))
+ // CRUD
+app.use('/api/admin', require('./controllers/crud')(models.Admin));
+app.listen( port, console.log(`port ${port} runinig`))
---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+
+const Admin = require('./Admin')
+const User = require('./User')
+const Account = require('./Account')
+const models = {
+    Account,
+    Admin,
+    User
+}
+module.exports = models
